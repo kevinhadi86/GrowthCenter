2d7bc0bf8d69bac3c9c62a1ad67c9bcdc422a43d
Jordan Leonardi <jordantheyinz@gmail.com>
2019-11-16 00:10:54 +0800

    feat: complete header

---
+++ resources/views/page/home.blade.php
@@ -0,0 +1,14 @@
+@extends("layout.master")
+
+@section("content")
+    <div class="jumbotron jumbotron-fluid" style="background-color: grey">
+
+    </div>
+    <div class="jumbotron jumbotron-fluid" style="background-color: blue">
+
+    </div>
+@endsection
+
+@section("css")
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+@endsection
