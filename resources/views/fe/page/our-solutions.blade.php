7299661375747460421321ee02ff5ffdb8750706
Jordan Leonardi <jordantheyinz@gmail.com>
2019-12-28 02:39:32 +0700

    scroll

--- resources/views/fe/page/our-solutions.blade.php
+++ resources/views/fe/page/our-solutions.blade.php
@@ -6,8 +6,24 @@
             <div class="col-7 gc-full-height gc-container-center pl-5">
                 <span class="gc-georgia gc-title gc-text-light-bold">What Question do You Have in Mind?</span>
             </div>
-            <div class="col-5">
-
+            <div class="col-5 gc-full-height">
+                <div class="pt-5 pb-5 gc-full-height position-relative">
+                    <div class="gradient-fade"></div>
+                    <div id="scroll-container" class="gc-full-height">
+                        <div class="empty"></div>
+                        @for($i = 0; $i < 20; $i++)
+                            <div class="solution mt-5 mb-5 solution-{{$i}}" data-id="{{$i}}">
+                                <div class="hr-slot"></div>
+                                <a class="gc-georgia gc-text-light-bold collapsed gc-no-decoration-link mt-3 mb-3 solution-title solution-title-{{$i}}" data-toggle="collapse" href="#collapseSolution{{$i}}" aria-controls="collapseSolution{{$i}}" data-href="{{route('our-solutions-detail')}}">How can I help my employees unlock their potential? ({{$i+1}})</a>
+                                <div class="collapse mt-3 mb-5 solution-collapsible" id="collapseSolution{{$i}}">
+                                    But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.
+                                </div>
+                                <div class="hr-slot"></div>
+                            </div>
+                        @endfor
+                        <div class="empty"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -17,4 +33,86 @@
 @section("css")
     <link rel="stylesheet" href="{{asset('css/our-solutions.css')}}">
     <link rel="stylesheet" href="{{asset('css/component/address-footer.css')}}">
+@endsection
+
+@section("script")
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+    <script>
+        $(function() {
+           $(".solution-collapsible").on("show.bs.collapse", function() {
+               $solution = $(this).closest('.solution');
+               $solution.find('.hr-slot').html("<hr/>");
+           });
+            $(".solution-collapsible").on("hide.bs.collapse", function() {
+                $solution = $(this).closest('.solution');
+                $solution.find('.hr-slot').html("");
+            });
+
+            // init controller
+            var controller = new ScrollMagic.Controller({
+                container: "#scroll-container",
+                pushFollowers: false,
+            });
+
+            $solutions = $('.solution');
+            $('.solution').each(function(key, el) {
+                $el = $(el);
+                const id = $el.data('id');
+                new ScrollMagic.Scene({triggerElement: ".solution-title-" + id, duration: "100"})
+                    .on('enter start', function() {
+                        $("#collapseSolution" + id).collapse('show');
+                    })
+                    .on('leave end', function() {
+                        $("#collapseSolution" + id).collapse('hide');
+                    })
+                    .addTo(controller);
+            });
+            $('#scroll-container').scroll("")
+            // new ScrollMagic.Scene({triggerElement: "#sec1"})
+            //     .setClassToggle("#high1", "active") // add class toggle
+            //     .addIndicators() // add indicators (requires plugin)
+            //     .addTo(controller);
+
+            // const itemCount = $('.solution').length;
+            // function toCenter() {
+            //     const mid = parseInt(itemCount/2);
+            //     $midElement = $($('.solution')[mid]);
+            //     $midElement.find('.solution-collapsible').collapse('show');
+            //     $height = $midElement.height()/2;
+            //     $containerHeight = $("#scroll-container").height()/2;
+            //     $("#scroll-container").scrollTop($midElement.offset().top - $containerHeight + $height);
+            //     console.log('done');
+            // }
+            // const MAX_PAUSE_TIME = 5;
+            // let pause = 0;
+            //
+            // setInterval(function() {
+            //     if (pause > 0) {
+            //         console.log(pause);
+            //         pause--;
+            //     }
+            // }, 100);
+            //
+            // toCenter();
+            // $("#scroll-container").on('mousewheel DOMMouseScroll', function(e) {
+            //     e.preventDefault();
+            //     // setTimeout(function() {
+            //     console.log('run');
+            //         if(pause === 0) {
+            //             pause = MAX_PAUSE_TIME;
+            //             $("#scroll-container").off('scroll');
+            //             $('.solution-collapsible').collapse('hide');
+            //             $el = $($('.solution')[0]);
+            //             $("#scroll-container .empty:last").before($el);
+            //             toCenter();
+            //         }
+            //     // }, 10);
+            // });
+            $('a').click(function(e) {
+                e.preventDefault();
+                window.location.href = $(this).data('href');
+            });
+        });
+    </script>
 @endsection
