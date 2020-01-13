0e7e2390af171a89d2e73d3542c57f8b32643391
kevin <kevinhadinata86@gmail.com>
2020-01-13 11:49:55 +0800

    fix team member delete

--- app/Http/Controllers/Cms/TestimonyController.php
+++ app/Http/Controllers/Cms/TestimonyController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Cms;
 
-use App\testimony;
+use App\Testimony;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
